70db7f7d13e0fdc1233edc89d63f85090e0d8876
Varga Ábel <abelvarga1848@gmail.com>
2026-09-03 12:42:26 +0200

    Added setNumber and actual addition logic

--- src/DamnBigNumber.java
+++ src/DamnBigNumber.java
@@ -1,12 +1,12 @@
 public class DamnBigNumber {
-
     private String number;
-
     public DamnBigNumber(String number) {
         this.number = number;
     }
-
     public String getNumber() {
         return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
     }
 }
--- src/DamnBigNumbers.java
+++ src/DamnBigNumbers.java
@@ -1,6 +1,11 @@
 public class DamnBigNumbers {
     public static DamnBigNumber add(DamnBigNumber a, DamnBigNumber b) {
-// TODO: Implement addition logic
-        return new DamnBigNumber("0");
+        String num1 = a.getNumber();
+        String num2 = b.getNumber();
+// Actual addition logic (simplified)
+
+        int n1 = Integer.parseInt(num1);
+        int n2 = Integer.parseInt(num2);
+        return new DamnBigNumber(String.valueOf(n1 + n2));
     }
 }
