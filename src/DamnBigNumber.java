bed7082a6eb72589aac5cbdbabf4f8f2c199b991
Sebestyén Milán <sebestyenmilan16@gmail.com>
2026-09-03 12:36:41 +0200

    Implement some methods

--- src/DamnBigNumber.java
+++ src/DamnBigNumber.java
@@ -1,12 +1,14 @@
 public class DamnBigNumber {
-
     private String number;
-
     public DamnBigNumber(String number) {
         this.number = number;
     }
-
     public String getNumber() {
         return number;
+    }
+    @Override
+    public String toString() {
+        return "DamnBigNumber: " + number;
+
     }
 }
--- src/DamnBigNumbers.java
+++ src/DamnBigNumbers.java
@@ -1,6 +1,8 @@
 public class DamnBigNumbers {
     public static DamnBigNumber add(DamnBigNumber a, DamnBigNumber b) {
-// TODO: Implement addition logic
-        return new DamnBigNumber("0");
+        String num1 = a.getNumber();
+        String num2 = b.getNumber();
+// Simple concatenation for demonstration (not actual addition)
+        return new DamnBigNumber(num1 + num2);
     }
 }
